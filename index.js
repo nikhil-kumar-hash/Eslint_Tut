c85460f2df45fb043f5833cbf42b8dc97b5f0fe2
Nikhil <nikhil.kumar@kreditzy.com>
2021-01-07 10:23:12 +0530

    Added rules disabling using Js
    Signed-off-by: Nikhil <nikhil.kumar@kreditzy.com>

---
+++ index.js
@@ -0,0 +1,15 @@
+/* eslint-disable quotes, semi*/
+
+function Print(){
+    
+    
+    const str = "I like React";
+
+    for ( let i = 0; i < 5; i++ ) {
+        // eslint-disable-next-line no-console
+        console.log(str)
+    }
+}
+
+
+Print();
