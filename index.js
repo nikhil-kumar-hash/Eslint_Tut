ce40ab702c848ff0c26112c6fc7f76b1b1083c31
Nikhil <nikhil.kumar@kreditzy.com>
2021-01-07 10:24:57 +0530

    Lint free js
    Signed-off-by: Nikhil <nikhil.kumar@kreditzy.com>

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function Print(){
     const str = "I like React";
 
     for ( let i = 0; i < 5; i++ ) {
-        
+        // eslint-disable-next-line no-console
         console.log(str)
     }
 }
